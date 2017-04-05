6bf369a1bb1fb887edc78529b13dea5fd6180e5b
vickey2020 <viveksharma.iisc@gmail.com>
2017-04-05 13:49:45 +0530

    stack

---
+++ app1/src/com/collection/StackArray.java
@@ -0,0 +1,73 @@
+package com.collection;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+class MyStack11 {
+	private int[] arr;
+	private int capacity;
+	private int top;
+
+	public MyStack11() {
+		this.capacity = 4;
+		this.arr = new int[capacity];
+		this.top = -1;
+	}
+
+	public boolean isStackEmpty() {
+		return (top == -1);
+	}
+
+	public boolean isStackFull() {
+		return (top == arr.length - 1);
+	}
+
+	public void push(int data) {
+		if (isStackFull()) {
+			stackCapacityDouble();
+			arr[++top] = data;
+			 System.out.println(data);
+		} else {
+			System.out.println(data);
+			arr[++top] = data;
+		}
+	}
+
+	private void stackCapacityDouble() {
+
+		int[] temp = new int[capacity * 2];
+		for (int i = 0; i < arr.length; i++) {
+			temp[i] = arr[i];
+		}
+		capacity = capacity * 2;
+		arr = temp;
+	}
+
+	public int pop() {
+		if (isStackEmpty())
+			System.out.println("Stack Underflow");
+		return arr[top--];
+	}
+
+}
+
+public class StackArray {
+
+	public static void main(String[] args) {
+		MyStack11 ms = new MyStack11();
+		ms.push(10);
+		ms.push(20);
+		ms.push(30);
+		ms.push(40);
+		ms.push(50);
+		ms.push(60);
+		ms.push(60);
+		ms.push(60);
+		ms.push(60);
+		ms.push(60);
+		ms.push(60);
+		ms.push(60);
+
+	}
+
+}
