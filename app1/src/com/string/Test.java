404e6c4f544cdbdcf2bfd8d42ac232e654d61c72
vickey2020 <viveksharma.iisc@gmail.com>
2017-06-14 13:39:50 +0530

    commit

---
+++ app1/src/com/string/Test.java
@@ -0,0 +1,17 @@
+package com.string;
+
+public class Test {
+	static int j =fun();
+	static int i =10;
+	static int fun()
+	{
+	return	i;
+	}
+
+	public static void main(String[] args) {
+		System.out.println(i);
+		System.out.println(j);
+
+	}
+
+}
